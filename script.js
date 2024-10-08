1877f0ae8f677596b1ee9d5e22c4c5ad05851786
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-10-08 15:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+function handle() {
+	let name = document.getElementById("name").value
+	let year = document.getElementById("year").value
+	let url = document.getElementById("url")
+	if(name&&year){
+	url.innerHTML = `https://localhost:8080/?name=${name}&year=${year}`
+	}
+	else if(!name){
+		url.innerHTML = `https://localhost:8080/?year=${year}`
+	}
+	else{
+		url.innerHTML = `https://localhost:8080/?name=${name}`
+	}
+}
